e28e278a5d9745cceb35d0e6d81ab94b773be9a1
Suvam Hazra <suvamhazra2003@gmail.com>
2026-05-28 15:39:53 +0530

    fixing deploy config

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import ExpenseContext from "./context/ExpenseContext.jsx";
 createRoot(document.getElementById("root")).render(
   <ExpenseContext>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </ExpenseContext>,
 );
